2601e98260f4934c9df87ef1026f36643818b893
Rearles <53582049+Rearles@users.noreply.github.com>
2022-02-08 10:33:02 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using P0_Coding_Challenge_CSharp;
+
+
+
+Reverse reverse = new Reverse();
+string test1 = "Happy Birthday";
+string test2 = "MANY THANKS";
+string test3 = "sPoNtAnEoUs";
+
+Console.WriteLine("");
+Console.WriteLine("Test 1: ");
+Console.WriteLine("Original String: " + test1);
+Console.WriteLine("Case Reversed String: " + reverse.ReverseCase(test1));
+
+Console.WriteLine("");
+Console.WriteLine("Test 2: ");
+Console.WriteLine("Original String: " + test2);
+Console.WriteLine("Case Reversed String: " + reverse.ReverseCase(test2));
+
+Console.WriteLine("");
+Console.WriteLine("Test 3: ");
+Console.WriteLine("Original String: " + test3);
+Console.WriteLine("Case Reversed String: " + reverse.ReverseCase(test3));
---
+++ Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P0_Coding_Challenge_CSharp
+{
+    public class Reverse
+    {
+        public string ReverseCase(string toreverse)
+        {
+            string toreturn = "";
+            foreach(char i in toreverse)
+            {
+                string k = i.ToString();
+                if (k.ToUpper() == k) //if the original character is upppercase
+                {
+                    toreturn += k.ToLower();
+                }
+                else
+                {
+                    toreturn += k.ToUpper();
+                }
+            }
+            return toreturn;
+        }
+    }
+}
